4bec1801ecf307ed202d2bd3d8c8c873b82d2738
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-04-24 23:38:25 +0530

    Creating the Questions

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import "../App.css";
+import { Questions } from "../helpers/Questions";
 
 const Quiz = () => {
   return (
-    <div>Quiz</div>
+    <div className='Quiz'></div>
   )
 }
 
---
+++ src/helpers/Questions.js
@@ -0,0 +1,18 @@
+export const Questions = [
+    {
+        prompt:"What is my name?",
+        optionA:"Yash",
+        optionB:"Yashika",
+        optionC:"Yuddha",
+        optionD:"Ash",
+        answer:"optionA",
+    },
+    {
+        prompt:"Which of this is not a programming language?",
+        optionA:"Python",
+        optionB:"JavaScript",
+        optionC:"MC-03",
+        optionD:"Java",
+        answer:"optionC",
+    }
+];

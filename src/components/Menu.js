6d8d17fed8562c4a9d56cb2e1a66b534d4056c07
Yash Vaidya <bobbynaldocr7@gmail.com>
2022-04-24 17:11:48 +0530

    Setting Up the UI

---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+const Menu = () => {
+  return (
+    <div className="Menu">
+      <input type="text" placeholder="Ex. John Smith" />
+      <button>Start Quiz</button>
+    </div>
+  );
+};
+
+export default Menu;
